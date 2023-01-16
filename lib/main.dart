e698a75fff3485be06727f709ba31a296608061e
Ra <rradjab@yahoo.com>
2023-01-17 01:37:56 +0400

    ra

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(const SatishApp());
@@ -32,6 +33,16 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {});
   }
 
+  int textButtonCount = 0;
+  String dropdownvalue = 'Item 1';
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,45 +53,167 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 9,
-                itemBuilder: (BuildContext context, int index) => Card(
-                  child: SizedBox(
-                    height: 500,
-                    child: Center(
-                      child: Column(children: [
-                        Image.asset('assets/images/$index.png'),
-                        Text("Windows $index"),
-                      ]),
-                    ),
-                  ),
-                ),
-              ),
+      body: ListView.separated(
+        itemCount: 5,
+        separatorBuilder: (_, __) => const Divider(),
+        itemBuilder: (context, index) {
+          final data = items[index];
+          return ListTile(
+            trailing: const Icon(Icons.arrow_forward_ios),
+            title: Text(
+              data,
+              style: const TextStyle(fontSize: 25.0),
             ),
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: 9,
-                itemBuilder: (BuildContext context, int index) => Card(
-                  child: Center(
-                    child: Column(children: [
-                      Image.asset('assets/images/$index.png'),
-                      Text("Windows $index"),
-                    ]),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+            },
+          );
+        },
       ),
+      // Padding(
+      //   padding: const EdgeInsets.all(10),
+      //   child: IntrinsicHeight(
+      //     child: Column(
+      //       crossAxisAlignment: CrossAxisAlignment.stretch,
+      //       children: [
+      //         TextButton(
+      //           onPressed: () {
+      //             setState(() {
+      //               textButtonCount++;
+      //             });
+      //           },
+      //           child: Text("Text $textButtonCount"),
+      //         ),
+      //         DropdownButton(
+      //           // Initial Value
+      //           value: dropdownvalue,
+
+      //           // Down Arrow Icon
+      //           icon: const Icon(Icons.keyboard_arrow_down),
+
+      //           // Array list of items
+      //           items: items.map((String items) {
+      //             return DropdownMenuItem(
+      //               value: items,
+      //               child: Text(items),
+      //             );
+      //           }).toList(),
+      //           // After selecting the desired option,it will
+      //           // change button value to selected value
+      //           onChanged: (String? newValue) {
+      //             setState(() {
+      //               dropdownvalue = newValue!;
+      //             });
+      //           },
+      //         ),
+      //         IconButton(
+      //           onPressed: () {
+      //             debugPrint("IconButton Pressed");
+      //           },
+      //           icon: const Icon(Icons.abc),
+      //         ),
+      //         InkWell(
+      //           // When the user taps the button, show a snackbar.
+      //           onTap: () {
+      //             ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      //               content: Text('Tap'),
+      //             ));
+      //           },
+      //           child: const Padding(
+      //             padding: EdgeInsets.all(12.0),
+      //             child: Text('Flat Button'),
+      //           ),
+      //         ),
+      //         PopupMenuButton(
+      //           onSelected: (value) {
+      //             debugPrint(value);
+      //           },
+      //           itemBuilder: (BuildContext bc) {
+      //             return const [
+      //               PopupMenuItem(
+      //                 value: '/hello',
+      //                 child: Text("Hello"),
+      //               ),
+      //               PopupMenuItem(
+      //                 value: '/about',
+      //                 child: Text("About"),
+      //               ),
+      //               PopupMenuItem(
+      //                 value: '/contact',
+      //                 child: Text("Contact"),
+      //               )
+      //             ];
+      //           },
+      //         ),
+      //         OutlinedButton(
+      //           style: OutlinedButton.styleFrom(
+      //             backgroundColor: Colors.black, //<-- SEE HERE
+      //           ),
+      //           onPressed: () {
+      //             debugPrint("Outlined");
+      //           },
+      //           child: const Text(
+      //             'Outlined Button',
+      //             style: TextStyle(fontSize: 40),
+      //           ),
+      //         ),
+      //         FloatingActionButton(
+      //           onPressed: _incrementCounter,
+      //           tooltip: 'Increment',
+      //           child: const Icon(Icons.add),
+      //         ),
+      //         FadeInImage.memoryNetwork(
+      //           placeholder: kTransparentImage,
+      //           width: 72,
+      //           height: 72,
+      //           image: 'https://picsum.photos/250?image=9',
+      //         ),
+      //         SizedBox(
+      //           width: 20,
+      //           height: 80,
+      //           child: Placeholder(
+      //             fallbackHeight: 25.0,
+      //           ),
+      //         ),
+      //         // Expanded(
+      //         //   child: ListView.builder(
+      //         //     shrinkWrap: true,
+      //         //     scrollDirection: Axis.horizontal,
+      //         //     itemCount: 9,
+      //         //     itemBuilder: (BuildContext context, int index) => Card(
+      //         //       child: SizedBox(
+      //         //         height: 500,
+      //         //         child: Center(
+      //         //           child: Column(children: [
+      //         //             Image.asset('assets/images/$index.png'),
+      //         //             Text("Windows $index"),
+      //         //           ]),
+      //         //         ),
+      //         //       ),
+      //         //     ),
+      //         //   ),
+      //         // ),
+      //         // Expanded(
+      //         //   child: ListView.builder(
+      //         //     shrinkWrap: true,
+      //         //     itemCount: 9,
+      //         //     itemBuilder: (BuildContext context, int index) => Card(
+      //         //       child: Center(
+      //         //         child: Column(children: [
+      //         //           Image.asset('assets/images/$index.png'),
+      //         //           Text("Windows $index"),
+      //         //         ]),
+      //         //       ),
+      //         //     ),
+      //         //   ),
+      //         // ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -112,6 +245,28 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+// Removing SecondRoute
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
